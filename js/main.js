ab65a4cd00b83d41db98e33aeb6afe67cec63999
Evan Hong <e.hong1424@gmail.com>
2023-02-13 22:39:19 +0000

    Added a submit event listener p1-2/7, added and changed html/css #7

--- js/main.js
+++ js/main.js
@@ -5,3 +5,18 @@ function imagePreview() {
   oldImg.src = newImg.value;
 }
 newPicture.addEventListener('input', imagePreview);
+/*
+var form = document.getElementById('form');
+function formSubmit() {
+  event.preventDefault();
+  var title = document.getElementById('title');
+  var photoInput = document.getElementById('photoInput');
+  var notes = document.getElementById('notes');
+  var newObj = {
+    title: title.value,
+    photoUrl: photoInput.value,
+    text: notes.value
+  };
+}
+form.addEventListener('submit', formSubmit);
+ */
