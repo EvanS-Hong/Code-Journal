58b2cf607de69ebafc938a529b4648eb22e90cac
Evan Hong <e.hong1424@gmail.com>
2023-02-14 19:59:20 +0000

    Added a renderEntry fuction to generate a DOm tree for every entry & fixed some erroes in main/data.js #3

--- js/data.js
+++ js/data.js
@@ -6,15 +6,15 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
-var entres = [];
+
 var previousEntriesJSON = localStorage.getItem('javascript-local-storage');
 function JsonSerialize() {
-  var entresJSON = JSON.stringify(entres);
-  localStorage.setItem('javascript-local-storage', entresJSON);
+  var dataJSON = JSON.stringify(data.entries);
+  localStorage.setItem('javascript-local-storage', dataJSON);
 }
 
 window.addEventListener('beforeunload', JsonSerialize);
 
 if (previousEntriesJSON !== null) {
-  entres = JSON.parse(previousEntriesJSON);
+  data.entries = JSON.parse(previousEntriesJSON);
 }
--- js/main.js
+++ js/main.js
@@ -9,7 +9,8 @@ newPicture.addEventListener('input', imagePreview);
 var nextEntryId = 0;
 var entres = [];
 var form = document.getElementById('form');
-function formSubmit() {
+
+function formSubmit(entry) {
   event.preventDefault();
   var title = document.getElementById('title');
   var photoInput = document.getElementById('photoInput');
@@ -25,3 +26,33 @@ function formSubmit() {
   form.reset();
 }
 form.addEventListener('submit', formSubmit);
+
+function renderEntry(entry) {
+  var li = document.createElement('li');
+
+  var div1 = document.createElement('div');
+  div1.setAttribute('class', 'row');
+  li.appendChild(div1);
+
+  var img = document.createElement('img');
+  img.setAttribute('class', 'column-half');
+  div1.appendChild(img);
+
+  var div2 = document.createElement('div');
+  div2.setAttribute('class', 'column-half');
+  div1.appendChild(div2);
+
+  var h2 = document.createElement('h2');
+  div2.appendChild(h2);
+
+  var p = document.createElement('p');
+  div2.appendChild(p);
+
+  return li;
+}
+
+var ul = document.querySelector('ul');
+for (var i = 0; i < (data.entries).length; i++) {
+  var newElement = renderEntry(data.entries[i]);
+  ul.appendChild(newElement);
+}
